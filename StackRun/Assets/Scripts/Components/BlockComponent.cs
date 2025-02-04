54fd4858cf73ebc800d4fb554cccc6e3b306c7f8
Omer <omeerfarukk12@gmail.com>
2025-02-04 14:12:50 +0300

    Block mechanics

--- StackRun/Assets/Scripts/Components/BlockComponent.cs
+++ StackRun/Assets/Scripts/Components/BlockComponent.cs
@@ -1,14 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Components
 {
     public class BlockComponent : MonoBehaviour
     {
-        [SerializeField] private float speed;
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private float moveSpeed = 2.5f;
+        [SerializeField] private float fallSpeed = 2.5f;
             
         private bool _isRight;
         private bool _isMoving;
+        private bool _isFalling;
         
         public void StartMoving(bool isRight)
         {
@@ -18,15 +22,43 @@ namespace Components
         
         private void Update()
         {
-            if (!_isMoving)
-                return;
-
-            transform.position += (_isRight ? Vector3.left : Vector3.right) * Time.deltaTime * speed;
+            if (_isMoving)
+            {
+                transform.position += (_isRight ? Vector3.left : Vector3.right) * Time.deltaTime * moveSpeed;
+            }
+            else if (_isFalling)
+            {
+                transform.position += Vector3.down * Time.deltaTime * fallSpeed;
+            }
         }
 
         public void StopMoving()
         {
             _isMoving = false;
+        }
+
+        public void PerfectPlace(float xPos)
+        {
+            var newPos = new Vector3(xPos, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+
+        public void StartFalling()
+        {
+            _isFalling = true;
+            Destroy(gameObject, 3);
+        }
+
+        public void SetMaterial(Material blockMaterial)
+        {
+            meshRenderer.sharedMaterial = blockMaterial;
+        }
+
+        public void SetXScale(float xScale)
+        {
+            var scale = transform.localScale;
+            scale.x = xScale;
+            transform.localScale = scale;
         }
     }
 }
--- StackRun/Assets/Scripts/Managers/BlockManager.cs
+++ StackRun/Assets/Scripts/Managers/BlockManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Components;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -12,8 +13,12 @@ namespace Managers
         [SerializeField] private Transform blockParent;
         [SerializeField] private float blockYPos = -0.55f;
         [SerializeField] private float horizontalSpace;
+        [SerializeField] private float xDiffToleration = 0.1f;
+        
+        [SerializeField] private List<Material> blockMaterials;
     
         private BlockComponent _previousBlock;
+        private Vector3 _defaultScale;
         private int _totalStackCount;
         private int _stackCounter;
         private bool _isRight;
@@ -22,11 +27,12 @@ namespace Managers
         private void Start()
         {
             inputManager.OnClick += OnClick;
+            _defaultScale = blockComponent.transform.localScale;
         }
 
         public void CreateFirstBlock()
         {
-            Instantiate(blockComponent, new Vector3(0f, blockYPos, 0f), Quaternion.identity, blockParent);
+            _previousBlock = Instantiate(blockComponent, new Vector3(0f, blockYPos, 0f), Quaternion.identity, blockParent);
         }
 
         public void SetTotalStackCount(int totalStackCount)
@@ -38,15 +44,17 @@ namespace Managers
         {
             while (_stackCounter < _totalStackCount)
             {
-                var positionZ = blockComponent.transform.localScale.z * (_stackCounter + 1);
+                var positionZ = _defaultScale.z * (_stackCounter + 1);
                 var newBlock = Instantiate(blockComponent,
                     new Vector3(_isRight ? horizontalSpace : -horizontalSpace, blockYPos, positionZ), Quaternion.identity,
                     blockParent);
+                newBlock.SetMaterial(blockMaterials[_stackCounter % blockMaterials.Count]);
+                newBlock.transform.localScale = _previousBlock.transform.localScale;
                 newBlock.StartMoving(_isRight);
                 
                 await UniTask.WaitUntil(() => _isClicked);
                 newBlock.StopMoving();
-                
+                CheckBlock(newBlock);
                 
                 _isClicked = false;
                 _stackCounter++;
@@ -57,6 +65,45 @@ namespace Managers
         private void OnClick()
         {
             _isClicked = true;
+        }
+
+        private void CheckBlock(BlockComponent newBlock)
+        {
+            var previousXPos = _previousBlock.transform.position.x;
+            var previousScaleX = _previousBlock.transform.localScale.x;
+            var xDiff = newBlock.transform.position.x - previousXPos;
+            Debug.Log("xDiff: " + xDiff);
+            if (Mathf.Abs(xDiff) < xDiffToleration)
+            {
+                Debug.Log("Tolerated by " + xDiff);
+                newBlock.PerfectPlace(previousXPos);
+                _previousBlock = newBlock;
+            }
+            else if (Mathf.Abs(xDiff) >= previousScaleX)
+            {
+                newBlock.StartFalling();
+            }
+            else
+            {
+                var fallingBlockPos = new Vector3(
+                    previousXPos + (xDiff / 2f) + (xDiff > 0 ? previousScaleX / 2f : -previousScaleX / 2f), blockYPos,
+                    newBlock.transform.position.z);
+                var fallingBlock = Instantiate(blockComponent, fallingBlockPos, Quaternion.identity, blockParent);
+                fallingBlock.SetMaterial(blockMaterials[_stackCounter % blockMaterials.Count]);
+                fallingBlock.SetXScale(previousScaleX - (previousScaleX - Mathf.Abs(xDiff)));
+                fallingBlock.StartFalling();
+                fallingBlock.name = "FallingBlock" + _stackCounter;
+
+                var remainingBlockPos =
+                    new Vector3(previousXPos + (xDiff / 2f), blockYPos, newBlock.transform.position.z);
+                var remainingBlock = Instantiate(blockComponent, remainingBlockPos, Quaternion.identity, blockParent);
+                remainingBlock.SetMaterial(blockMaterials[_stackCounter % blockMaterials.Count]);
+                remainingBlock.SetXScale(previousScaleX - Mathf.Abs(xDiff));
+                remainingBlock.name = "RemainingBlock" + _stackCounter;
+                
+                Destroy(newBlock.gameObject);
+                _previousBlock = remainingBlock;
+            }
         }
 
         private void OnDestroy()
